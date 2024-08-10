6299cb0a913ed0cd13208efb93feab28335a2c00
OleksandrKirkov <kirkovsasha009@gmail.com>
2024-08-10 22:09:09 +0000

    feat: add search.lua

---
+++ lua/base/search.lua
@@ -0,0 +1,18 @@
+-- [[ Variable ]] --
+
+-- Nvim options
+local opt = vim.opt
+
+-- Nvim global
+local g = vim.g
+
+-- [[ Search ]] --
+
+-- Search TEXT result text
+opt.ignorecase = true
+
+-- Search Text result Text
+opt.smartcase = true
+
+-- Highlight found text objects
+opt.showmatch = true
